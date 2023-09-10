f5f3d18a543559cd6bd035a5118b342c4629ce33
Soledad Shukuya <soledadshukuya@hotmail.com>
2023-09-10 19:25:26 -0300

    Agregado header responsive

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+// HEADER
+const hamburguesa = document.querySelector(".menu__hamburguesa");
+const cruz = document.querySelector(".hamburguesa__cruz");
+const menuLista = document.querySelector(".menu__lista");
+
+hamburguesa.addEventListener("click", () => {
+    hamburguesa.classList.toggle("active");
+    cruz.classList.toggle("active");
+    menuLista.classList.toggle("active");
+}); 
+
+cruz.addEventListener("click", () => {
+    hamburguesa.classList.toggle("active");
+    cruz.classList.toggle("active");
+    menuLista.classList.toggle("active");
+}); 
